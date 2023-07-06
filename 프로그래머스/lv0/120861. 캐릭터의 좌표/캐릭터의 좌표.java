6949f85d8a4ecbcc2f57fe231658bcf7696a669d
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-06 15:26:22 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[2];
+        int x = board[0] / 2;
+        int y = board[1] / 2;
+        
+        for(String s : keyinput) {
+            if(s.equals("up")) answer[1] += 1;
+            else if(s.equals("down")) answer[1] -= 1;
+            else if(s.equals("left")) answer[0] -= 1;
+            else if(s.equals("right")) answer[0] += 1;
+            
+            if(answer[0] > 0 && answer[0] > x) answer[0] = x;
+            else if(answer[0] < 0 && answer[0] < -x) answer[0] = -x;
+                
+            if(answer[1] > 0 && answer[1] > y) answer[1] = y;
+            else if(answer[1] < 0 && answer[1] < -y) answer[1] = -y;
+        }
+        
+        return answer;
+    }
+}
