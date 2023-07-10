0f9a84f2a4e42e2da67eb1a8e7aa31d4dda5abc5
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-10 17:41:44 +0900

    Update ［1차］ 비밀지도.java
    설명 주석 추가

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,7 +3,13 @@ class Solution {
         String[] answer = new String[n];
         
         for (int i = 0; i < n; i++) {
+            // toBinaryString로 2진수로 변경
+            // |함수를 사용하여 한 쪽이 1이면 1이 출력되고 그 외에는 0을 출력하도록 함
+            // String.format는 문자열 형식을 설정하는 메서드임
+            // %s는 문자열 형식을 설정하겠다는 의미로 중간에 n의 숫자를 넣었을 때 n보다 문자열의 길이가 짧으면 앞에 공백을 추가함
             answer[i] = String.format("%"+n+"s", Integer.toBinaryString(arr1[i] | arr2[i]));
+
+            // 0은 ' '공백으로, 1은 벽인 "#"로 한꺼번에 변경 처리
             answer[i] = answer[i].replace('0', ' ').replace('1', '#');
         }
         return answer;
