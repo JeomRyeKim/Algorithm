c261e088958a42808b0b5abc8c2b9418b06637cf
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-12 12:15:38 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int n) {
-        StringBuffer sb = new StringBuffer(Integer.toString(n, 3)).reverse(); 
+        StringBuilder sb = new StringBuilder(Integer.toString(n, 3)).reverse(); 
         return Integer.parseInt(String.valueOf(sb),3);
     }
 }
