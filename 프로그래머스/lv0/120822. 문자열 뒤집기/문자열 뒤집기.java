44876fec654b2e999783625954b1a612cb5e8ce0
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-14 21:19:18 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] str = my_string.split("");
-        
-        for(int i = str.length-1; i >= 0; i--)
-            answer += str[i];
-        
-        return answer;
+        StringBuilder sb = new StringBuilder(my_string).reverse();
+        return sb.toString();
     }
 }
