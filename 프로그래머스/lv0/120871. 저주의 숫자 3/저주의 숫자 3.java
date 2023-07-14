14d0c53081d0350981b5bb73c0e442ecd994d292
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-14 16:26:58 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.08 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,13 +1,21 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
+
         for(int i = 1; i <= n; i++) {
             answer++;
-            if(answer % 3 == 0 || String.valueOf(answer).contains("3"))
-                i--;
+            if (i % 3 == 0) {
+                if(answer % 3 == 0) answer++;
+            }
+            
+            if(String.valueOf(answer).contains("3")) {
+                String strAns = String.valueOf(answer);
+                strAns = strAns.replace("3", "4");
+                answer = Integer.parseInt(strAns);
+            }
+
+            if(answer % 3 == 0) answer++;
         }
-        
         return answer;
     }
 }
