c79155257623d6866f81fccea41881c9fff99345
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-17 17:12:26 +0900

    [level 0] Title: 특이한 정렬, Time: 0.45 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        Integer[] arr = new Integer[numlist.length];
+        for (int i = 0; i < numlist.length; i++) {
+            arr[i] = numlist[i];
+        }
+
+        Arrays.sort(arr, new Comparator<Integer>() {
+            public int compare(Integer n1, Integer n2) {
+                if (Math.abs(n1 - n) < Math.abs(n2 - n)) {
+                    return -1;
+                } else if (Math.abs(n1 - n) > Math.abs(n2 - n)) {
+                    return 1;
+                } else if(Math.abs(n1 - n) == Math.abs(n2 - n)) {
+                    return n1 > n2 ? -1 : 1;
+                } else return 0;
+            }
+        });
+
+        for (int i = 0; i < numlist.length; i++) {
+            numlist[i] = arr[i];
+        }
+
+        return numlist;
+    }
+}
