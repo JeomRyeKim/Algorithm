c2e8be0fac9e585a3f24478c0ce7aff7b175b164
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-20 12:16:24 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int[] common) {
+        return common[2] - common[1] == common[1] - common[0] 
+            ? common[common.length-1] + (common[1] - common[0]) 
+            : common[common.length-1] * (common[1] / common[0]);
+    }
+}
