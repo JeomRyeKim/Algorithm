4c38343cb00d203a526fc5f4de4c8791d512ea70
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-22 00:59:37 +0900

    [level 0] Title: 특이한 정렬, Time: 0.53 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -3,9 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numlist, int n) {
         Integer[] arr = new Integer[numlist.length];
-        for (int i = 0; i < numlist.length; i++) {
-            arr[i] = numlist[i];
-        }
 
         Arrays.sort(arr, new Comparator<Integer>() {
             public int compare(Integer n1, Integer n2) {
