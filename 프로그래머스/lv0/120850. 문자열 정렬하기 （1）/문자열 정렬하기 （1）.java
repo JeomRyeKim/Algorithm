d26f7481007f9e30b2e0b15e4539d32ce15ebf1a
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-23 23:43:29 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.04 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,11 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(String my_string) {
-        String str = "";
-        for(char c : my_string.toCharArray()) {
-            if(c >= '0' && c <= '9') str += c;
-        }
-        
-        return Arrays.stream(str.split("")).sorted().mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
     }
 }
