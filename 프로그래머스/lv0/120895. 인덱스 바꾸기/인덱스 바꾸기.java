48fcec20f62ecd54e445508955706d03bcf67fb9
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-24 16:19:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.22 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] str = my_string.split("");
+        String tmp = str[num2];
+        str[num2] = str[num1];
+        str[num1] = tmp;
+        
+        return String.join("", str);
+    }
+}
