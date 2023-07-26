b13e61d9f805bb31fce0d87c274c6ea2030e2e9e
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-07-26 11:15:52 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        
+        for(char c : String.valueOf(x).toCharArray()) {
+            sum += (int)c - '0';
+        }
+                
+        return x % sum == 0 ? true : false;
+    }
+}
