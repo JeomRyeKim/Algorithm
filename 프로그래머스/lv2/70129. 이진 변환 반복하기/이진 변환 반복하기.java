bfce5f90fd1c11a14e2fe0061dae5e07e53bdebf
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-02 14:30:00 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 49.55 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
         
+        
         while(!s.equals("1")) {
             int cnt = 0;
             for(String str : s.split("")) {
