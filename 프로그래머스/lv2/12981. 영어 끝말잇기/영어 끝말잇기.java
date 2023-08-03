be82abc5f21ed7b1f23ee648d18e291a6aac2945
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-03 12:27:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.37 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0, 0};
+        
+        for(int i=1; i<words.length; i++) {
+            boolean wordFound = false;
+            for (int j = 0; j < i; j++) {
+                if (words[j].equals(words[i])) {
+                    wordFound = true;
+                    break;
+                }
+            }
+            
+            char lastWord = words[i-1].charAt(words[i-1].length() - 1);
+            char firstWord = words[i].charAt(0);
+            
+            if(lastWord != firstWord || wordFound) {
+                answer[0] = (i % n) + 1;
+                answer[1] = (int) Math.ceil((double)(i + 1) / n);
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
