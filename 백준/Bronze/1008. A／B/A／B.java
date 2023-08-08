c4e7996e7919d5d9d1c1060325cf80580fade458
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-08 19:56:27 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] ars) {
+        Scanner in = new Scanner(System.in);
+        double A = in.nextInt();
+        double B = in.nextInt();
+        System.out.println(A / B);
+    }
+}
