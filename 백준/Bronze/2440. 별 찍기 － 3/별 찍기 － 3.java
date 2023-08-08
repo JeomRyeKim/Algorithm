fc75a3288551a7e52348d8afabb2f5fe377fab11
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-09 00:46:59 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] ars) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+
+        for(int i=A; i>0; i--) {
+            System.out.println("*".repeat(i));
+        }
+    }
+}
