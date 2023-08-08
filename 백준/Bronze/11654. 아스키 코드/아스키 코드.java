94fa9b3e43ea03e224220900d2d662fe243e3282
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-08 23:37:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] ars) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+
+        System.out.println((byte) A.charAt(0));
+    }
+}
