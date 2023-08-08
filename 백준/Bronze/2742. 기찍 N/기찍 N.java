5cac26a11b288ae8356364823a604f20b987b78a
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-08 20:59:51 +0900

    [Bronze IV] Title: 기찍 N, Time: 1064 ms, Memory: 32196 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] ars) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+
+        for(int i=A; i >= 1; i--) System.out.println(i);
+    }
+}
