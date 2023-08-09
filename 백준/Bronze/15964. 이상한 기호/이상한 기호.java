d5120c9efae3a6730ff164027b300e39ba98db66
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-09 23:42:27 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,10 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] ars) {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
+    public static void main(String[] ars) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
         System.out.println((A+B)*(A-B));
     }
