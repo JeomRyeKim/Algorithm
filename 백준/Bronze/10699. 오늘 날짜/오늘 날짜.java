515ded75f5b9feb545593d6e257f2a2de8d94228
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-09 21:30:18 +0900

    [Bronze V] Title: 오늘 날짜, Time: 268 ms, Memory: 18692 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,8 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] ars) {
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+    }
+}
