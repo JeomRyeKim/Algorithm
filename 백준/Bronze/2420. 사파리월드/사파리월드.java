39fd432f3e49dffd249092f6410fb1f3a586113a
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-09 21:37:22 +0900

    [Bronze V] Title: 사파리월드, Time: 220 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] ars) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(Math.abs(in.nextLong() - in.nextLong()));
+    }
+}
