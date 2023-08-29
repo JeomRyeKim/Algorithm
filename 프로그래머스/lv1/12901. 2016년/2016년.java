74affd7420b0c3aa856e510ecac6fba084e64039
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-29 11:41:37 +0900

    [level 1] Title: 2016년, Time: 1.22 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -2,19 +2,17 @@ import java.time.*;
 
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
         int day = LocalDate.of(2016, a, b).getDayOfWeek().getValue();
         
         switch(day) {
-            case 1 : answer = "MON"; break;
-            case 2 : answer = "TUE"; break;
-            case 3 : answer = "WED"; break;
-            case 4 : answer = "THU"; break;
-            case 5 : answer = "FRI"; break;
-            case 6 : answer = "SAT"; break;
-            case 7 : answer = "SUN"; break;
-        }
-        
-        return answer;
+            case 1 : return "MON";
+            case 2 : return "TUE";
+            case 3 : return "WED";
+            case 4 : return "THU";
+            case 5 : return "FRI";
+            case 6 : return "SAT";
+            case 7 : return "SUN";
+            default: return "";
+        } 
     }
 }
