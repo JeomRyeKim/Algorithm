5ae85ca6f7ddb5fa30a68c238b5f830dac0a512d
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-08-31 11:50:58 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -8,11 +8,9 @@ class Solution {
         for(int i : ingredient) {
             arr[idx++] = i;
             
-            if(idx >= 4) {
-                if(arr[idx-4] == 1 && arr[idx-3] == 2 && arr[idx-2] == 3 && arr[idx-1] == 1){
-                    answer++;
-                    idx -= 4;
-                }
+            if(idx >= 4 && arr[idx-4] == 1 && arr[idx-3] == 2 && arr[idx-2] == 3 && arr[idx-1] == 1){
+                answer++;
+                idx -= 4;
             }
         }
 
