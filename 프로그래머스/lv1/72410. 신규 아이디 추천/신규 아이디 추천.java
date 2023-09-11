445ed4e5639df4bed37cc0293bd848bbe740297f
이혜정 <90684309+JeomRyeKim@users.noreply.github.com>
2023-09-11 12:27:34 +0900

    [level 1] Title: 신규 아이디 추천, Time: 11.59 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        
+        // 1단계
+        new_id = new_id.toLowerCase();
+        
+        // 2단계
+        for(char c : new_id.toCharArray()) {
+           if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.')
+               answer += c;
+        }
+        
+        // 3단계
+        while(answer.contains("..")) {
+            answer = answer.replace("..", ".");
+        }
+        
+        // 4단계
+        if(answer.startsWith(".")) 
+            answer = answer.substring(1, answer.length());
+        else if (answer.endsWith("."))
+            answer = answer.substring(0, answer.length()-1);
+        
+        // 5단계
+        if(answer.equals("")){
+            answer = "aaa";
+        }
+        
+        // 6단계
+        if(answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+        }
+        // 6.5단계
+        if(answer.startsWith(".")) 
+            answer = answer.substring(1, answer.length());
+        else if (answer.endsWith("."))
+            answer = answer.substring(0, answer.length()-1);
+        
+        
+        // 7단계
+        if(answer.length() <= 2) {
+            while(answer.length() < 3)
+                answer += answer.charAt(answer.length()-1);
+        }
+        
+        return answer;
+    }
+}
